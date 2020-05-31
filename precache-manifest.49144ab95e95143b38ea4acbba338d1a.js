c8b600e792a80a560d28b98a913b527d3fdb34dd
Shibly <shibly2016@gmail.com>
2020-05-31 07:36:41 +0530

    Updates

---
+++ precache-manifest.49144ab95e95143b38ea4acbba338d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e11bab358452251e0ca30835b646ef",
+    "url": "/promodoclock/index.html"
+  },
+  {
+    "revision": "b5ca22d03e40f4ec2bf6",
+    "url": "/promodoclock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ebb489f6faa68cc2cc3",
+    "url": "/promodoclock/static/js/2.a98d668f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/promodoclock/static/js/2.a98d668f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ca22d03e40f4ec2bf6",
+    "url": "/promodoclock/static/js/main.60ec3ea0.chunk.js"
+  },
+  {
+    "revision": "97c6a9a5659af0be6678",
+    "url": "/promodoclock/static/js/runtime-main.892d5871.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/promodoclock/static/media/logo.5d5d9eef.svg"
+  }
+]);
